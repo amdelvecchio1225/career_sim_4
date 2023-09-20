d037163a7b95edfa5f7fa4840722e13c898b800d
Anthony DelVecchio <amdelvecchio1225@gmail.com>
2023-09-20 19:51:02 -0400

    finished registration setup, installed needed dependancies & packages, ignored env

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,4 +1,30 @@
 const router = require("express").Router();
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
+router.post("/register", async(req, res, next) => {
+    
+    const salt_rounds = 5;
+
+    const hashedPassword = await bcrypt.hash(req.body.password, salt_rounds);
+
+    try {
+        const user = await prisma.user.create({
+            data: {
+                username: req.body.username,
+                password: hashedPassword
+            }
+        })
+
+        const token = jwt.sign({id: user.id}, process.env.JWT);
+
+        res.status(201).send({token, user:{userId: user.id, username: user.username}});
+
+    } catch(err) {
+        next(err);
+    }
+})
+
+module.exports = router;
